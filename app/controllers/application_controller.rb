8ba2748e45d59de5ce6117955d39bf8cc5c944c5
Yuri <ycr.data@gmail.com>
2024-10-16 17:38:06 -0300

    implementing jwt decoding to validate tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  SECRET_KEY = ENV['JWT_SECRET'] || 'secret'
+
+  def decode_token
+    token = request.headers['Authorization']&.split(' ')&.last
+    JWT.decode(token, SECRET_KEY)[0]
+  rescue JWT::DecodeError
+    nil
+  rescue JWT::ExpiredSignature
+    nil
+  end
+
+  def current_user
+    decoded = decode_token
+    return nil unless decoded
+
+    @current_user ||= User.find_by(id: decoded['user_id']) if decoded
+  end
+
+  def authenticate_user!
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless current_user
+  end
 end
