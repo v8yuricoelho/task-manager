ac57851233e13db2f0aaec079afaa206dd52264b
Yuri <ycr.data@gmail.com>
2024-10-18 10:34:03 -0300

    removing update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,16 +12,6 @@ class TasksController < ApplicationController
     end
   end
 
-  def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      render json: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
-
   private
 
   def task_params
